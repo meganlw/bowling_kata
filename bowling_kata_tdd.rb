78503902035f81bbce042477d19a3b532dbb717b
Megan Watson <megan.watson@madetech.com>
2022-10-19 10:46:10 +0100

    Add second test that passes

--- bowling_kata_tdd.rb
+++ bowling_kata_tdd.rb
@@ -1,11 +1,22 @@
 class Bowling
 
+    def initialize
+        @rolls = []
+    end
     def roll pins
-
+        @rolls << pins
     end
 
     def score
-        0
+        # @rolls.reduce(:+)  /reduce method does same thing as below
+
+        result = 0
+        rollIndex = 0
+        20.times do 
+            result += @rolls[rollIndex]
+            rollIndex += 1
+        end
+        result
     end
 end
 
@@ -13,16 +24,18 @@ end
 
 describe "bowling_game" do
 
-
-    
-    it 'can make a new game' do
-        game = Bowling.new
+    before do
+        @game = Bowling.new
     end
 
     it 'can roll gutter game' do
-        game = Bowling.new
-        20.times{game.roll(0)}
-        expect(game.score).to eq(0)
+        20.times{@game.roll(0)}
+        expect(@game.score).to eq(0)
+    end
+
+    it 'can roll all ones' do
+        20.times{@game.roll(1)}
+        expect(@game.score).to eq(20)
     end
 
 
