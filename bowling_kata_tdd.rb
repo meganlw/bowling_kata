593453c46f469721e722e62daef5673395e9c760
Megan Watson <megan.watson@madetech.com>
2022-10-19 10:28:32 +0100

    first tests complete

--- bowling_kata_tdd.rb
+++ bowling_kata_tdd.rb
@@ -1,3 +1,29 @@
+class Bowling
+
+    def roll pins
+
+    end
+
+    def score
+        0
+    end
+end
+
+
+
 describe "bowling_game" do
+
+
+    
+    it 'can make a new game' do
+        game = Bowling.new
+    end
+
+    it 'can roll gutter game' do
+        game = Bowling.new
+        20.times{game.roll(0)}
+        expect(game.score).to eq(0)
+    end
+
 
 end
